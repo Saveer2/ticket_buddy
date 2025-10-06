3ef9d218c0cde773948768a8e38eb0ca73edaa31
SaveerMore <98229535+Saveer2@users.noreply.github.com>
2025-10-07 00:55:42 +0530

    Delete index1.js

--- index1.js
+++
@@ -1,9 +0,0 @@
-const switcher = document.getElementById('languageSwitcher');
-
-switcher.addEventListener('change', () => {
-  const selectedPage = switcher.value;
-  if (selectedPage) {
-    // Force reload even if already on the selected page
-    window.location.href = selectedPage + '?refresh=' + new Date().getTime();
-  }
-});
